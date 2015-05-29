f389d16ce3ab589f5826eff85f546c560d7161a4
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2015-05-29 12:09:44 +0900

    コメント

--- app/src/main/java/org/nunocky/modelviewpresenterstudy01/RootModule.java
+++ app/src/main/java/org/nunocky/modelviewpresenterstudy01/RootModule.java
@@ -6,7 +6,7 @@ import dagger.Module;
 import dagger.Provides;
 
 @Module(
-        injects = MainActivityFragment.class
+        injects = {MainActivityFragment.class} // 複数クラスへの injectは {}で囲む
 )
 public class RootModule {
     @Provides
